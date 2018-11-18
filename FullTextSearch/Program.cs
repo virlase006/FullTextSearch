10590b7c886abc04ea70dbe8f84e7301bdb92481
virlase006 <virla.devi@gmail.com>
2018-11-18 12:10:37 -0500

    add resource file

--- FullTextSearch/Program.cs
+++ FullTextSearch/Program.cs
@@ -56,17 +56,17 @@ namespace FullTextSearch
 
         private static void PerformSearch()
         {
-            Console.Write("Enter text you would like to search: ");
+            Console.Write(Resource1.EnterSearchText);
             var text = Console.ReadLine();
             var result = DbHelper.Search(text, con);
             foreach (var item in result)
             {
                 Console.WriteLine($"{item.Key} : {item.Value}");
             }
-            Console.WriteLine("What would you like to do next?");
-            Console.WriteLine("1- Search another text");
-            Console.WriteLine("2- Go back to start screen");
-            Console.WriteLine("Any key to exit");
+            Console.WriteLine(Resource1.ChooseAction);
+            Console.WriteLine(Resource1.SearchMore);
+            Console.WriteLine(Resource1.GoBack);
+            Console.WriteLine(Resource1.AnyOtherKey);
             var resp = Console.ReadLine();
             if (resp == "1")
             {
@@ -84,21 +84,21 @@ namespace FullTextSearch
 
         private static void PerformCrudOpertaion()
         {
-            Console.WriteLine("Choose the table you would like to perform CRUD Operations");
-            Console.WriteLine("1- Sports News");
-            Console.WriteLine("2- Weather News");
+            Console.WriteLine(Resource1.ChooseTable);
+            Console.WriteLine(Resource1.SportsTable);
+            Console.WriteLine(Resource1.WeatherTable);
             var table = Console.ReadLine();
             if (table == "1" || table == "2")
             {
                 var tableName = table == "1" ? sportsNewTable : weatherNewsTable;
-                Console.WriteLine("Chose CRUD");
+                Console.WriteLine(Resource1.ChooseCRUD);
                 Console.WriteLine("1-Create");
                 Console.WriteLine("2-Update");
                 Console.WriteLine("3-Delete");
                 var resp = Console.ReadLine();
                 PerfromCRUD(tableName, resp);
             }
-            else Console.WriteLine("Invalid Selection");
+            else Console.WriteLine(Resource1.InvalidSelection);
 
 
         }
@@ -109,25 +109,25 @@ namespace FullTextSearch
             {
                 case "1":
                     CreateNewRecord(tableName);
-                    Console.WriteLine("Created record");
+                    Console.WriteLine(Resource1.CreateRecord);
                     break;
                 case "2":
                     UpdateRecord(tableName);
-                    Console.WriteLine("Updated record");
+                    Console.WriteLine(Resource1.UpdatedRecorded);
                     break;
                 case "3":
                     DeleteRecord(tableName);
-                    Console.WriteLine("Deleted record");
+                    Console.WriteLine(Resource1.DeletedRecord);
                     break;
                 default:
-                    Console.WriteLine("Invalid options");
+                    Console.WriteLine(Resource1.InvalidSelection);
                     break;
             }
         }
 
         private static void DeleteRecord(string tableName)
         {
-            Console.WriteLine("Enter record ID you would like to delete");
+            Console.WriteLine(Resource1.EnterRecIdForDelete);
             var recordId = Console.ReadLine();
             DbHelper.ExecuteCommand($"DELETE from  {tableName} WHERE id = {recordId};", con);
 
@@ -135,44 +135,49 @@ namespace FullTextSearch
 
         private static void CreateNewRecord(string tableName)
         {
-            Console.WriteLine("Enter heading:");
+            Console.WriteLine(Resource1.EnterValueForCOlumn.Replace("_columnName_","heaeding"));
             var heading = Console.ReadLine();
-            Console.WriteLine("Enter news: ");
+            Console.WriteLine(Resource1.EnterValueForCOlumn.Replace("_columnName_", "news"));
+
             var news = Console.ReadLine();
             DbHelper.ExecuteCommand($"INSERT INTO {tableName} (news, heading) VALUES ('{news}','{heading}');", con);
         }
 
         private static void UpdateRecord(string tableName)
         {
-            Console.WriteLine("Enter record Id you would like  to edit");
+            Console.WriteLine(Resource1.EnterRecIdForUpdate);
             var recordId = Console.ReadLine();
-            Console.WriteLine("Choose column you would like to update \n 1- news \n 2- heading");
+            Console.WriteLine(Resource1.ChooseColumn);
             var column = Console.ReadLine();
 
             if (column == "1" || column == "2")
             {
                 var columnName = column == "1" ? "news" : "heading";
-                Console.WriteLine($"Enter new value for the {columnName}");
+                Console.WriteLine(Resource1.EnterValueForCOlumn.Replace("_columnName_",columnName));
                 var newValue = Console.ReadLine();
                 Console.WriteLine($"UPDATE  {tableName} SET {columnName}='{newValue}' WHERE id={recordId};");
                 DbHelper.ExecuteCommand($"UPDATE  {tableName} SET {columnName}='{newValue}' WHERE id={recordId};", con);
+            }
+            else
+            {
+                Console.WriteLine(Resource1.InvalidSelection);
             }
 
         }
 
         private static void StartingScreen()
         {
-            Console.WriteLine("Choose from given options");
-            Console.WriteLine("1- Initialize database");
-            Console.WriteLine("2- Perform CRUD operations");
-            Console.WriteLine("3- Full text search");
+            Console.WriteLine(Resource1.ChooseAction);
+            Console.WriteLine(Resource1.InitializeDb);
+            Console.WriteLine(Resource1.PerformCRUD);
+            Console.WriteLine(Resource1.SearchText);
             var response = Console.ReadLine();
             PerformAction(DbHelper, con, response);
         }
 
         private static void InitializeDatabaseForSearching(DBHelper dbHelper, SQLiteConnection con)
         {
-            Console.WriteLine("Choose what you want to do:\n 1- Create tables and indecis \n 2- Batch insert");
+            Console.WriteLine(Resource1.ChooseDBActions);
             var resp = Console.ReadLine();
             if (resp == "2" || resp == "1")
             {
@@ -186,7 +191,7 @@ namespace FullTextSearch
             }
             else
             {
-                Console.WriteLine("Invalide Operation");
+                Console.WriteLine(Resource1.InvalidSelection);
             }
             
         }
